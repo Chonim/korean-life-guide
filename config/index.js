dc0460375945821cedc8104e5a3158ec2492f46f
Chonim <chihoyoon1217@gmail.com>
2019-08-11 19:24:22 +0900

    moidy publicPath

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/korean-life-guide',
+    assetsPublicPath: '/korean-life-guide/',
 
     /**
      * Source Maps
