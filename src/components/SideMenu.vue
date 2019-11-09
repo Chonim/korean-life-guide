37bebee645f9a4836df5a7d4a6926c2a0cea7d4c
Chonim <chihoyoon1217@gmail.com>
2019-11-09 12:16:50 +0900

    add children routes and modify css

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -2,15 +2,34 @@
   <div class="side-menu-wrapper">
     <div id="mySidenav" class="sidenav">
       <a href="#" class="closebtn" @click="closeNav()">&times;</a>
-      <a
-        v-for="menu in menus"
-        :key="menu.route"
-        @click="routerPush(menu.route)"
-      >
-        {{ translates[menu.pageName] }}
-      </a>
+      <ul>
+        <li
+          v-for="menu in menus"
+          :key="menu.pageName"
+        >
+          <a @click="routerPush(menu.route)">
+            {{ translates[menu.pageName] }}
+          </a>
+          <ul>
+            <li
+              v-for="child in menu.children"
+              :key="child.pageName"
+              class="child-route"
+            >
+              <a @click="routerPush(child.route)">
+                - {{ translates[child.pageName] }}
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
     </div>
     <div class="sidenav-toggle" @click="openNav()">&#9776;</div>
+    <div
+      v-show="isNavOpen"
+      class="mask"
+      @click="closeNav()"
+    />
   </div>
 </template>
 
@@ -19,18 +38,42 @@ export default {
   name: 'SideMenu',
   data () {
     return {
+      isNavOpen: false,
       menus: [{
         pageName: '가까운 기관 찾기',
         route: '/'
       }, {
+        pageName: '비자',
+        route: '/visa',
+        children: [{
+          pageName: 'E-7-4',
+          route: ''
+        }, {
+          pageName: 'D-10-1',
+          route: ''
+        }, {
+          pageName: 'F-2',
+          route: ''
+        }]
+      }, {
+        pageName: '노동',
+        route: '/',
+        children: [{
+          pageName: '기숙사',
+          route: '/dormitory'
+        }, {
+          pageName: '사업장 변경',
+          route: '/'
+        }, {
+          pageName: '육아도우미 채용절차',
+          route: '/'
+        }]
+      }, {
+        pageName: '외국인 생활안내',
+        route: '/'
+      }, {
         pageName: '함께 하는 사람들',
         route: '/about'
-      }, {
-        pageName: '비자',
-        route: '/visa'
-      }, {
-        pageName: '기숙사 관련',
-        route: '/dormitory'
       }]
     }
   },
@@ -46,9 +89,11 @@ export default {
     },
     openNav () {
       document.getElementById('mySidenav').style.left = '0px'
+      this.isNavOpen = true
     },
     closeNav () {
       document.getElementById('mySidenav').style.left = '-250px'
+      this.isNavOpen = false
     }
   }
 }
@@ -64,23 +109,29 @@ export default {
     z-index: 1;
     top: 0;
     left: -250px;
-    background-color: #111;
+    background-color: #fff;
     overflow-x: hidden;
     transition: left 0.3s ease-in-out;
     padding-top: 60px;
-  }
-
-  .sidenav a {
-    padding: 8px 8px 8px 32px;
-    text-decoration: none;
-    font-size: 25px;
-    color: #818181;
-    display: block;
-    transition: 0.3s;
-  }
-
-  .sidenav a:hover {
-    color: #f1f1f1;
+    li:hover {
+      background-color: #fff;
+    }
+    a {
+      padding: 8px 8px 8px 32px;
+      text-decoration: none;
+      font-size: 16px;
+      color: #202020;
+      display: block;
+      transition: 0.3s;
+      &:hover {
+        background-color: #f6f6f6;
+      }
+    }
+    .child-route {
+      a {
+        padding-left: 48px;
+      }
+    }
   }
 
   .sidenav .closebtn {
@@ -102,6 +153,14 @@ export default {
 @media screen and (max-height: 450px) {
   .sidenav {padding-top: 15px;}
   .sidenav a {font-size: 18px;}
+}
+
+.mask {
+  position: fixed;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.6);
 }
 
 </style>
