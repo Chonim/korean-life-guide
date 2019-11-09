2bcabadfb82bc8890ce2858884a43dc728e07956
Chonim <chihoyoon1217@gmail.com>
2019-11-09 20:55:04 +0900

    store date in the project instead of firebase

---
+++ src/assets/data/index.js
@@ -0,0 +1,9 @@
+import immigrantsSupport from './immigrants_support.json'
+import immigrationOffices from './immigration_offices.json'
+import recruitCenter from './recruit_center.json'
+
+export default [
+  ...immigrantsSupport,
+  ...immigrationOffices,
+  ...recruitCenter
+]
